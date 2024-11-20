10bbe98358499249a448eb1d1a3ded140bc2008c
Justin White <j.white0513@gmail.com>
2024-11-20 17:51:21 -0500

    Move sql to choreboard-backend

---
+++ Backend/choreboard-backend/sql/choreboard.sql
@@ -0,0 +1,49 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS choreboard;
+
+USE choreboard;
+
+-- Create the Users table
+CREATE TABLE IF NOT EXISTS Users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('ADMIN', 'PARENT', 'CHILD') NOT NULL,
+    email VARCHAR(100),
+    points INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create the Chores table
+CREATE TABLE IF NOT EXISTS Chores (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    points INT DEFAULT 0,
+    assigned_to BIGINT,
+    status VARCHAR(50) DEFAULT 'PENDING',
+    due_date VARCHAR(255),
+    completed_date DATE,
+    FOREIGN KEY (assigned_to) REFERENCES Users(id)
+);
+
+-- Create the Rewards table
+CREATE TABLE IF NOT EXISTS Rewards (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    points INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Create the Redemptions table
+CREATE TABLE IF NOT EXISTS Redemptions (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT,
+    reward_id BIGINT,
+    redeemed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     description VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (reward_id) REFERENCES rewards(id)
+);
