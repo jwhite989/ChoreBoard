5f992a1bdb724f7ada82ffecf46f36b28494f4de
Justin White <j.white0513@gmail.com>
2024-11-20 20:39:36 -0500

    setup backend for deployment

--- Backend/choreboard-backend/src/main/java/com/example/choreboard_backend/config/SecurityConfig.java
+++ Backend/choreboard-backend/src/main/java/com/example/choreboard_backend/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList(System.getenv().getOrDefault("FRONTEND_URL", "http://localhost:4200")));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
